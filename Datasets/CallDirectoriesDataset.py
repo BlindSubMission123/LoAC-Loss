6e9fad192894ac7ca9b56a3d1364455cf9e9e72d
BlindSubMission123 <komunokenzaken@gmail.com>
2026-07-13 17:10:27 -0400

    Update CallDirectoriesDataset.py

--- Datasets/CallDirectoriesDataset.py
+++ Datasets/CallDirectoriesDataset.py
@@ -1,8 +1,19 @@
 def call_directories_dataset(dataset):
     if dataset == 'PH2':
         # Train
-        DATA_PATH_TRAIN_IMG = r'/home/max/datasets/PH2_NewOrder/Images'
-        DATA_PATH_TRAIN_MASK = r'/home/max/datasets/PH2_NewOrder/Masks'
+        DATA_PATH_TRAIN_IMG = r'/home/.../datasets/PH2_NewOrder/Images'
+        DATA_PATH_TRAIN_MASK = r'/home/.../datasets/PH2_NewOrder/Masks'
+        # Validation
+        DATA_PATH_VAL_IMG = ''
+        DATA_PATH_VAL_MASK = ''
+        # Test
+        DATA_PATH_TEST_IMG = ''
+        DATA_PATH_TEST_MASK = ''
+
+    if dataset == 'CellTracking':
+        # Train 
+        DATA_PATH_TRAIN_IMG = r'/home/.../datasets/DatasetsCellTracking/Images'
+        DATA_PATH_TRAIN_MASK = r'/home/.../datasets/DatasetsCellTracking/GroundTruth'
         # Validation
         DATA_PATH_VAL_IMG = ''
         DATA_PATH_VAL_MASK = ''
